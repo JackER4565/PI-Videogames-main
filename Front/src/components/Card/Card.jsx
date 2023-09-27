5c8b30811e0d613c85f2d984d5159fba39da69c4
Fabrizio Rondinella <fabrondinella@gmail.com>
2023-09-27 17:04:33 -0300

    cards

--- Front/src/components/Card/Card.jsx
+++ Front/src/components/Card/Card.jsx
@@ -1,16 +1,12 @@
 import styles from "./Card.module.css";
-// import { useState, useEffect } from "react";
 
 import { Link } from "react-router-dom";
 
 export default function Card({ id, name, image, genre, currentPage }) {
-
 	return (
 		<Link to={`/detail/${id}&${currentPage}`}>
 			<div className={styles.container}>
-				<div className={styles.text}>
-					<h2>{name}</h2>
-				</div>
+				<h2 className={styles.text}>{name}</h2>
 				<img
 					className={styles.image}
 					src={image}
@@ -29,4 +25,3 @@ export default function Card({ id, name, image, genre, currentPage }) {
 		</Link>
 	);
 }
-
